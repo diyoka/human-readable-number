2ad8e3d7d09de6a4817668f9eaff4a50dc4f7860
Diyora Kattakhodjaeva <100277878+diyoka@users.noreply.github.com>
2022-07-12 13:48:53 +0500

    fix: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function toReadable (number) {
+var nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+var tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+module.exports = function toReadable (number)
+ { var digit = number % 10; 
+    if (number < 20)
+     { return nums[number]; }
+      else if (number < 100) 
+      { return tens[(number/10)-2] + (digit? " " + nums[digit]: ""); }
+       else { return nums[(number/100)] +" hundred" + (number%100 === 0? "": " " + toReadable(number%100)); } }
   
-}
